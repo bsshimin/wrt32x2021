f0b803b95fe3feb44f0e4a49c1f7a5bcf850dc65
bsshimin <66291290+bsshimin@users.noreply.github.com>
2021-03-14 16:01:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/bsshimin/lede/tree/master/package/lede' feeds.conf.default
+git clone https://github.com/bsshimin/lede/tree/master/package/lede package/luci-app-ssr-plus
