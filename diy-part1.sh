ef16c64fa11062953a7be5d1d59c8d1b0c06e8e0
bsshimin <66291290+bsshimin@users.noreply.github.com>
2021-03-16 10:17:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git bsshimin https://github.com/bsshimin/lede' feeds.conf.default
-#src-git small https://github.com/kenzok8/small
-#src-git kenzo https://github.com/kenzok8/openwrt-packages
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git bsshimin https://github.com/bsshimin/lede' feeds.conf.default
+# src-git small https://github.com/kenzok8/small
+# src-git kenzo https://github.com/kenzok8/openwrt-packages
 # git clone https://github.com/bsshimin/lede/tree/master/package/lean lean/luci-app-ssr-plus
