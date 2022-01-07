ceeb5129407e2ce72708e7338341c0befb853009
bsshimin <66291290+bsshimin@users.noreply.github.com>
2022-01-08 07:57:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@
 # src-git kenzo https://github.com/kenzok8/openwrt-packages
 # git clone https://github.com/bsshimin/lede/tree/master/package/lean lean/luci-app-ssr-plus
 git clone https://github.com/fw876/helloworld package/helloworld
-git clone https://github.com/bsshimin/lean package/lean
+# git clone https://github.com/bsshimin/lean package/lean
 # git clone https://github.com/bsshimin/hack-4.14 target/linux/generic/hack-5.4
